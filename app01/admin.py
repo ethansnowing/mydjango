1e97fd8b129ab19ef0b76c81a02c8524e1f37d4a
yudehuai@xunlei.com <ethansnowing>
2018-11-17 15:19:27 +0800

    2、表格参数拼写错误纠正

--- app01/admin.py
+++ app01/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from app01 import models
 # Register your models here.
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title','category','author','pub_date','last_modify','status','priority')
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('article','parent_comment','comment_type','comment','user','date')
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name','set_as_top_menu','position_index')
+
+admin.site.register(models.Article)
+admin.site.register(models.Comment)
+admin.site.register(models.UserProfile)
+admin.site.register(models.Category)
--- app01/models.py
+++ app01/models.py
@@ -7,17 +7,18 @@ from django.core.exceptions import ValidationError
 class Article(models.Model):
     title = models.CharField(max_length=255)
     brief = models.CharField(null=True,blank=True,max_length=255)
-    category = models.ForeignKey("Category")
+    category = models.ForeignKey("Category", on_delete=models.CASCADE)
     content = models.TextField(u"文章内容")
-    author = models.ForeignKey("UserProfile")
+    author = models.ForeignKey("UserProfile", on_delete=models.CASCADE)
     pub_date = models.DateTimeField(blank=True,null=True)
     last_modify = models.DateTimeField(auto_now=True)
     priority = models.IntegerField(u"优先级",default=1000)
+    head_img = models.ImageField(u"文章标题图片", upload_to='uploads')
     status_choices = (('draft',u"草稿"),
                       ('published',u"已发布"),
                       ('hidden',u"隐藏"))
-    status = models.CharField(choices=status_choices,default='published')
-    def __unicode__(self):
+    status = models.CharField(choices=status_choices,default='published',max_length=255)
+    def __str__(self):
         return self.title
     def clean(self):
         if self.status == 'draft' and self.pub_date is not None:
@@ -27,37 +28,37 @@ class Article(models.Model):
 
 
 class Comment(models.Model):
-    article = models.ForeignKey(Article,verbose_name=u"所属文章")
-    parent_comment = models.ForeignKey('self',related_name='my_children',blank=True,null=True)
+    article = models.ForeignKey(Article,verbose_name=u"所属文章", on_delete=models.CASCADE)
+    parent_comment = models.ForeignKey('self',related_name='my_children',blank=True,null=True,on_delete=models.CASCADE)
     comment_choices = ((1,u'评论'),
                        (2,u'点赞'))
-    comment_type = models.IntergerField(choices=comment_choices,default=1)
-    user = models.ForeignKey(auto_now_add=True)
+    comment_type = models.IntegerField(choices=comment_choices,default=1)
+    user = models.ForeignKey("UserProfile",on_delete=models.CASCADE)
     comment = models.TextField(blank=True,null=True)
     date = models.DateTimeField(auto_now_add=True)
     def clean(self):
-        if self.comment_type ==1 and self.comment is None:
+        if self.comment_type ==1 and len(self.comment)==0:
             raise ValidationError('评论不能为空')
 
-    def __unicode__(self):
+    def __str__(self):
         return "%s,P:%s,%s" %(self.articel,self.parent_comment.id,self.comment)
 
 class Category(models.Model):
     name = models.CharField(max_length=64)
     brief = models.CharField(null=True,blank=True,max_length=255)
     set_as_top_menu = models.BooleanField(default=False)
-    position_index = models.SmallIntergerField()
+    position_index = models.SmallIntegerField()
     admins = models.ManyToManyField("UserProfile",blank=True)
-    def __unicode__(self):
+    def __str__(self):
         return self.name
 
 class UserProfile(models.Model):
-    user = models.OneToOneField(User)
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
     name = models.CharField(max_length=32)
     signature = models.CharField(max_length=255,blank=True,null=True)
     head_img = models.ImageField(height_field=150,width_field=150,blank=True,null=True)
 
-    def __unicode__(self):
+    def __str__(self):
         return self.name
 
 
