d821d0c332d413aa6667e740524b52b6e4994d97
ethansnowing <udehi163@sina.com>
2018-12-10 00:24:56 +0800

    增加httpurl到数据库

--- app01/views.py
+++ app01/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
 from app01 import models
+import os
 # Create your views here.
 
 category_list = models.Category.objects.filter(set_as_top_menu=True).order_by('position_index')
 video_list = models.Video.objects.order_by('name')
 def index(request):
+    f = open('httpurl.txt','rb')
+    for line in f.readlines():
+        httpurl = line.decode('utf-8')
+        name = os.path.split(httpurl)[1][:-2]
+        # print(name, httpurl)
 
+        # dic = {"name":name,"httpurl":httpurl}
+        # models.Video.objects.create(**dic)
+    f.close()
+    # models.Video.objects.filter(id=5).delete()        # 数据库列删除
     return render(request,'app01/index.html', {'category_list':category_list,
                                                'video_list':video_list})
 
